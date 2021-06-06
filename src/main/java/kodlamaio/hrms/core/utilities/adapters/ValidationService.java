6d425235c8212ab1fffa396f12435e8b9236c013
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2021-06-06 14:41:04 +0300

    Delete src directory

--- src/main/java/kodlamaio/hrms/core/utilities/adapters/ValidationService.java
+++
@@ -1,5 +0,0 @@
-package kodlamaio.hrms.core.utilities.adapters;
-
-public interface ValidationService {
-	boolean validateByMernis(long nationalId, String firstName, String lastName, int yearOfBirth);
-}
--- src/main/java/kodlamaio/hrms/core/verification/VerificationService.java
+++
@@ -1,6 +0,0 @@
-package kodlamaio.hrms.core.verification;
-
-public interface VerificationService {
-	void sendLink(String email);
-	String sendCode();
-}
