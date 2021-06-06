7592544756b3b00866de9763eeb25e76b2611e37
Yasin Şensoy <73845925+sensoyyasin@users.noreply.github.com>
2021-06-06 14:37:53 +0300

    Delete HRMSproject directory

--- HRMSproject/src/main/java/kodlama/HRMSproject/dataAccess/abstracts/JobPositionDao.java
+++
@@ -1,9 +0,0 @@
-package kodlama.HRMSproject.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlama.HRMSproject.entities.concretes.JobPosition;
-
-public interface JobPositionDao extends JpaRepository<JobPosition,Integer>{
-
-}
